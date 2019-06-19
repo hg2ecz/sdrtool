40fc57950b5d6e082af7af1067215d39d247c035
Zsolt Krüpl <hg2ecz@ham.hu>
2019-06-19 18:15:39 +0000

    set RTL_SDR freq from stdin

---
+++ src/command_in.rs
@@ -0,0 +1,39 @@
+use std::io::{self, BufRead};
+use std::thread;
+use std::time;
+use std::sync::mpsc;
+
+pub struct CmdIn {
+    rx: mpsc::Receiver<Result<String, io::Error>>,
+}
+
+impl CmdIn {
+    pub fn new() -> CmdIn {
+        let (tx, rx) = mpsc::channel();
+        thread::spawn(move || {
+            let input = io::stdin();
+            for line in input.lock().lines() {
+                tx.send(line).unwrap();
+            }
+        });
+        return CmdIn {rx: rx};
+    }
+
+    // f-0.3  --> set mixfreq to -0.3 MHz
+    pub fn get(&self) -> Option<(char, f64)> {
+        let d = time::Duration::from_millis(0);
+        if let Ok(val) = self.rx.recv_timeout(d) {
+            let s_in = val.unwrap();    // get...
+            let s_in = s_in.trim_end();
+            let c: char = s_in.chars().next().unwrap();
+            let s: &str = &s_in[1..];
+            if let Ok(sf64) = s.parse() {
+                Some((c, sf64))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
