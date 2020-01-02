eb8ee8729184e78f041f743665c887e63adafa44
Zsolt Krüpl <hg2ecz@ham.hu>
2020-01-02 00:52:54 +0000

    +MSI SDR s16

--- src/io.rs
+++ src/io.rs
@@ -54,6 +54,40 @@ impl Sdrtcpcli {
         resvec
     }
 
+    pub fn read_s16(&mut self) -> Vec<Complex<f32>> {
+        let mut resvec: Vec<Complex<f32>> = vec![];
+        let mut buffer = [0u8; 1 << (13+1)];
+        self.tcpstream.read_exact(&mut buffer).unwrap();
+        for i in (0..buffer.len()).step_by(2*2) {
+	    let mut num1: i32 = buffer[i+1] as i32 * 256 + buffer[i+0] as i32 ;
+            let mut num2: i32 = buffer[i+3] as i32 * 256 + buffer[i+2] as i32 ;
+            if num1 >= 0x8000 { num1 -= 0x10000 };
+            if num2 >= 0x8000 { num2 -= 0x10000 };
+            resvec.push(Complex::new(
+                num1 as f32 / 64.,
+                num2 as f32 / 64.,
+            ));
+        }
+        resvec
+    }
+
+    pub fn read_f32(&mut self) -> Vec<Complex<f32>> {
+        let mut resvec: Vec<Complex<f32>> = vec![];
+        let mut buffer = [0u8; 1 << (13+2)];
+        self.tcpstream.read_exact(&mut buffer).unwrap();
+        let mut tmp1 = [0; 4];
+        let mut tmp2 = [0; 4];
+        for i in (0..buffer.len()).step_by(4*2) {
+            tmp1.copy_from_slice(&buffer[i .. i+4]);
+            tmp2.copy_from_slice(&buffer[i+4 .. i+8]);
+            resvec.push(Complex::new(
+                f32::from_ne_bytes(tmp1),
+                f32::from_ne_bytes(tmp2)
+            ));
+        }
+        resvec
+    }
+
     pub fn write_u8(&mut self, data: &[u8]) {
         self.tcpstream.write_all(&data).unwrap();
     }
